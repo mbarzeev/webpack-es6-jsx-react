73a68fcefd3718c6037035f8f878f5ac41ffe5f4
Matti Bar-Zeev <flashmattic@gmail.com>
2015-10-06 14:32:25 +0300

    Initial commit

---
+++ entry.js
@@ -0,0 +1,9 @@
+'use strict';
+
+import React from 'react';
+import HelloReact from './hello-react'
+
+React.render(
+  <HelloReact />,
+  document.body
+);
---
+++ hello-react.js
@@ -0,0 +1,13 @@
+'use strict';
+
+import React from 'react';
+
+class HelloReact extends React.Component {
+  render() {
+    return <div><h1>Hello, REACT :)</h1>
+      < div > Dave ? What are you doing Dave...?</div>
+      < /div>;
+  }
+}
+
+export default HelloReact;
